534bbe980a4ceadc359de6545645d1c96f108a6d
Harkor421 <sekesie2@gmail.com>
2021-02-03 18:24:40 -0500

    V1.7

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (message.content.startsWith(prefix + "help")){
     .setColor('#fffafa')
     .setTitle("**Glossary**")
     .setAuthor('Albertico', '', '')
-    .setDescription(`**1. Measurements and uncertainties (coming soon)** \n\n **2. Mechanics -----> _mech** \n\n **3. Thermal Physics (coming soon)** \n\n **4. Waves (coming soon)** \n\n **5. Electricity and magnetism (coming soon)** \n\n **6. Circular motion and gravitation (coming soon)** \n\n **7. Atomic, nuclear and particle physics (coming soon)** \n\n **8. Energy production**`)
+    .setDescription(`**1. Measurements and uncertainties (coming soon)** \n\n **2. Mechanics -----> _mech** \n\n **3. Thermal Physics (coming soon)** \n\n **4. Waves (coming soon)** \n\n **5. Electricity and magnetism (coming soon)** \n\n **6. Circular motion and gravitation (coming soon)** \n\n **7. Atomic, nuclear and particle physics (coming soon)** \n\n **8. Energy production* (coming soon)*`)
    
 message.channel.send(glossary);
 }
@@ -78,6 +78,21 @@ if (message.content.startsWith(prefix + "mech")) {
         .setDescription("**v = Velocity | t = Time | s = Distance Travelled | a = Acceleration | u = Initial Velocity | F = Force | m = Mass | P = Power | f = Friction | N = Normal | W = Work | p = Momentum | j = Impulse | E,p = Potential Energy | E,k = Kinetic Energy| k = Spring Constant | w = Weight**")
         .setImage('https://media.discordapp.net/attachments/758102244905582613/801191183828385832/unknown.png?width=213&height=293')
 
+        const work = new MessageEmbed() 
+        .setColor('#fffafa')
+        .setTitle("Work Formulae")
+        .setAuthor('MECHANICS', '', '')
+        .setDescription("**v = Velocity | t = Time | s = Distance Travelled | a = Acceleration | u = Initial Velocity | F = Force | m = Mass | P = Power | f = Friction | N = Normal | W = Work | p = Momentum | j = Impulse | E,p = Potential Energy | E,k = Kinetic Energy| k = Spring Constant | w = Weight**")
+        .setImage('https://media.discordapp.net/attachments/484471959623696394/806663219438682153/unknown.png?width=177&height=194')
+
+        const power = new MessageEmbed() 
+        .setColor('#fffafa')
+        .setTitle("Work Formulae")
+        .setAuthor('MECHANICS', '', '')
+        .setDescription("**v = Velocity | t = Time | s = Distance Travelled | a = Acceleration | u = Initial Velocity | F = Force | m = Mass | P = Power | f = Friction | N = Normal | W = Work | p = Momentum | j = Impulse | E,p = Potential Energy | E,k = Kinetic Energy| k = Spring Constant | w = Weight**")
+        .setImage('https://media.discordapp.net/attachments/484471959623696394/806666113005387836/unknown.png?width=113&height=156')
+
+
         const emojiList = ["◀️", "▶️"];
 
         const timeout = '120000'; 
@@ -87,7 +102,9 @@ if (message.content.startsWith(prefix + "mech")) {
             initial,
             velocity,
             acceleration,
-            time
+            time,
+            work,
+            power
         ];
 
         paginationEmbed(message, pages, emojiList, timeout);
